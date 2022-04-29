bfa76fe61209dcb3ff869c3a6cdf0bc013280033
kerbalturbo102 <72495729+kerbalturbo102@users.noreply.github.com>
2022-04-29 19:48:39 +0900

    test

---
+++ src/public/js/app.js
@@ -0,0 +1,11 @@
+const socket = io();
+
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+function handleRoomSubmit() {
+    event
+}
+
+form.addEventListener("submit", handleRoomSubmit);
---
+++ src/server.js
@@ -0,0 +1,49 @@
+import http from "http";
+import SocketIO from "socket.io"
+import express from "express";
+
+const app = express()
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+
+const httpServer = http.createServer(app);
+const io = SocketIO(httpServer);
+
+
+io.on("connection", socket => {
+  console.log(socket)
+})
+
+
+
+function onSocketClose() {
+    console.log("Disconnected from the Browser ❌");
+}
+
+// const sockets = [];
+// const wss = new WebSocket.Server({ server });
+// wss.on("connection", (socket) => {
+//     sockets.push(socket);
+//     socket["nickname"] = "Anon";
+//     console.log("Connected to Browser ✅");
+//     socket.on("close", onSocketClose);
+//     socket.on("message", (msg) => {
+//         const message = JSON.parse(msg);
+//         switch (message.type) {
+//           case "new_message":
+//             sockets.forEach((aSocket) =>
+//               aSocket.send(`${socket.nickname}: ${message.payload}`)
+//             );
+//           case "nickname":
+//             socket["nickname"] = message.payload;
+//         }
+//       });
+// });
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
