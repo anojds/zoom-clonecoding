fc5a54a5841082440b9c0d00d64c3b818a691dee
kerbalturbo102 <72495729+kerbalturbo102@users.noreply.github.com>
2022-05-05 14:14:19 +0900

    1

--- src/server.js
+++ src/server.js
@@ -1,94 +1,17 @@
-import http from "http";
-import { Server } from "socket.io"
+import SocketIO from "socket.io";
 import express from "express";
-import { instrument } from "@socket.io/admin-ui";
 
-const app = express()
-
+const app = express();
 app.set("view engine", "pug");
+import http from "http";
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
-
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
-const io = new Server(httpServer, {
-  cors: {
-    origin: ["https://admin.socket.io"],
-    credentials: true
-  }
-});
-instrument(io, {
-  auth: false
-})
 
-function publicRooms() {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = io;
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
-}
-
-
-io.on("connection", (socket) => {
-  socket["nickname"] = `익명 #${Math.floor(Math.random()*9999)+1000}`;
-
-  socket.on("enter_room", (roomName, done) => {
-    socket.join(roomName)
-    done();
-    socket.to(roomName).emit("welcome", socket.nickname);
-    io.sockets.emit("room_change", publicRooms());
-  });
-
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname))
-  });
-  socket.on("disconnect", () => {
-    io.sockets.emit("room_change", publicRooms());
-  });
-
-  socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-    done();
-  });
-
-  socket.on("nickname", nickname => socket["nickname"] = nickname)
-})
-
-
-
-function onSocketClose() {
-    console.log("Disconnected from the Browser ❌");
-}
-
-// const sockets = [];
-// const wss = new WebSocket.Server({ server });
-// wss.on("connection", (socket) => {
-//     sockets.push(socket);
-//     socket["nickname"] = "Anon";
-//     console.log("Connected to Browser ✅");
-//     socket.on("close", onSocketClose);
-//     socket.on("message", (msg) => {
-//         const message = JSON.parse(msg);
-//         switch (message.type) {
-//           case "new_message":
-//             sockets.forEach((aSocket) =>
-//               aSocket.send(`${socket.nickname}: ${message.payload}`)
-//             );
-//           case "nickname":
-//             socket["nickname"] = message.payload;
-//         }
-//       });
-// });
+const wsServer = SocketIO(httpServer);
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
